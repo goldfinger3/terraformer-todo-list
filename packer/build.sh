9c7f42f2807cd3264dfeab797c01abfc25eb5480
Adron Hall <adronhall@gmail.com>
2019-08-08 17:54:27 -0700

    Blurble so much of this IS NOT WORKING!

--- packer/build.sh
+++ packer/build.sh
@@ -1,3 +1,22 @@
-'yes' | terraform apply
+GROUPNAME="adrons-images"
+LOCATION="westus2"
+STORAGENAME="adronsimagestorage"
+
+echo 'Creating the managed resource group for images.'
+
+az group create --name $GROUPNAME --location $LOCATION
+
+echo 'Creating the storage account for image storage.'
+
+az storage account create \
+ --name $STORAGENAME --resource-group $GROUPNAME \
+ --location $LOCATION \
+ --sku Standard_LRS \
+ --kind Storage
+
+echo 'Building Apache cluster node image.'
+
+# az ad sp create-for-rbac -n "Packer" --role contributor \
+#                             --scopes /subscriptions/{SubID}
 
 packer build node.json
